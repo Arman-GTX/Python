851b8b4910c7376dfc1f694de1d5058c3acf4086
Arman-GTX <armanaman986@gmail.com>
2025-08-03 03:20:19 +0530

    Add files via upload

---
+++ Dice_Rolling_Game.py
@@ -0,0 +1,12 @@
+import random
+choice=input('Roll the dice? (y/n): ').lower()
+if choice == 'y':
+    die1=random.randint(1, 6)
+    die2=random.randint(1, 6)
+    print(f'({die1}, {die2})')
+elif choice == 'n':
+    print('Thanks For Playing')
+   
+else:
+    print('Invalid Input')
+    
